a233d3874e89f633144d972b8c0cd95a3681820d
LAB13_USER ON WS32 <L13Y09W32@LabHCI.net>
2026-02-07 08:09:00 +0800

    Initializing of springboot backend

---
+++ backend/src/main/java/com/it342/g1/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.it342.g1.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/it342/g1/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it342.g1.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
